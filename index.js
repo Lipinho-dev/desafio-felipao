1c07cfb2d3015eeeeb2fc0b4e0b638da8dcdd03e
Lipinho-dev <luisfel050907@gmail.com>
2026-02-10 22:17:09 -0300

    feito os paranaue pra designar a patente

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("tamo aprendendo tamo aprendendo")
+const nomeDoHeroi = "Felipe";
+let xpDoHeroi = 6758;
+let patente = ''
+
+if (xpDoHeroi <= 1000) {
+    patente= "Ferro";
+} else if (xpDoHeroi <= 2000) {
+  patente = "Bronze";
+} else if (xpDoHeroi <= 5000) {
+  patente = "Prata";
+} else if (xpDoHeroi <= 7000) {
+  patente = "Ouro";
+} else if (xpDoHeroi <= 8000) {
+  patente = "Platina";
+} else if (xpDoHeroi <= 9000) {
+  patente = "Ascendente";
+} else if (xpDoHeroi <= 10000) {
+  patente = "Imortal";
+} else {
+  patente = "Radiante";
+}
+console.log("O herói de chama " + nomeDoHeroi + ", e tem " + xpDoHeroi + " de experiência e sua patente é: " + patente)
